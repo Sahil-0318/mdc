55945d434584fcd3f6425a8618281be9453316af
Sahil Kumar <sahilkc3440@gmail.com>
2024-05-14 21:30:45 +0530

    pdf filepath

--- controllers/recordRoomController.js
+++ controllers/recordRoomController.js
@@ -1,8 +1,8 @@
 import User from '../models/userModel/userSchema.js'
 import AdmissionForm from '../models/userModel/admissionFormSchema.js'
 import clcSchema from '../models/userModel/clcSchema.js'
-// import path from "path"
-import * as path from 'path'
+import path from "path"
+// import * as path from 'path'
 import { PDFDocument, StandardFonts, rgb } from "pdf-lib"
 import { writeFileSync, readFileSync, existsSync, unlink } from "fs"
 
@@ -123,7 +123,7 @@ const printCertificate = async (req, res) => {
     try {
         const { certificate, course, id } = req.params
         // console.log(certificate, course, id );
-        
+
         const user = await User.findOne({ _id: req.id })
         let foundCertificate = ''
         const currentDate = new Date();
@@ -138,163 +138,162 @@ const printCertificate = async (req, res) => {
 
 
         if (certificate === 'clc') {
-            await clcSchema.findOneAndUpdate({ _id: id}, { $set: { dOLC: issueDate } })
-            foundCertificate = await clcSchema.findOneAndUpdate({ _id: id}, { $set: { isIssued: true } })
+            await clcSchema.findOneAndUpdate({ _id: id }, { $set: { dOLC: issueDate } })
+            foundCertificate = await clcSchema.findOneAndUpdate({ _id: id }, { $set: { isIssued: true } })
 
             let reversedDOB = reverseString(foundCertificate.dOB);
-        let reversedDOAdm = reverseString(foundCertificate.dOAdm);
-
-        
-        //Create pdf
-        const document = await PDFDocument.load(readFileSync("./CLC.pdf"));
-        const courierBoldFont = await document.embedFont(StandardFonts.Courier);
-        const firstPage = document.getPage(0);
-
-        firstPage.moveTo(105, 629);
-        firstPage.drawText(`${foundCertificate.serialNo}`, {
-            font: courierBoldFont,
-            size: 16,
-        });
-
-        firstPage.moveTo(431, 630);
-        firstPage.drawText(` ${foundCertificate.studentId}`, {
-            font: courierBoldFont,
-            size: 16,
-        });
-
-        firstPage.moveTo(350, 604);
-        firstPage.drawText(`${foundCertificate.fullName}`, {
-            font: courierBoldFont,
-            size: 12,
-        });
-
-        firstPage.moveTo(350, 576);
-        firstPage.drawText(`${foundCertificate.fatherName}`, {
-            font: courierBoldFont,
-            size: 12,
-        });
-
-        firstPage.moveTo(350, 550);
-        firstPage.drawText(`${foundCertificate.motherName}`, {
-            font: courierBoldFont,
-            size: 12,
-        });
-
-        firstPage.moveTo(350, 523);
-        firstPage.drawText(`${foundCertificate.aadharNumber}`, {
-            font: courierBoldFont,
-            size: 12,
-        });
-
-        firstPage.moveTo(350, 497);
-        firstPage.drawText(`${foundCertificate.parmanentAddress}`, {
-            font: courierBoldFont,
-            size: 12,
-        });
-
-        firstPage.moveTo(350, 471);
-        firstPage.drawText(`${reversedDOB}`, {
-            font: courierBoldFont,
-            size: 12,
-        });
-
-        firstPage.moveTo(350, 444);
-        firstPage.drawText(`${foundCertificate.course}`, {
-            font: courierBoldFont,
-            size: 12,
-        });
-
-        firstPage.moveTo(350, 417);
-        firstPage.drawText(`${foundCertificate.session}`, {
-            font: courierBoldFont,
-            size: 12,
-        });
-
-        firstPage.moveTo(350, 390);
-        firstPage.drawText(`${reversedDOAdm}`, {
-            font: courierBoldFont,
-            size: 12,
-        });
-
-        firstPage.moveTo(350, 363);
-        firstPage.drawText(`${foundCertificate.classRollNumber}`, {
-            font: courierBoldFont,
-            size: 12,
-        });
-
-        firstPage.moveTo(350, 336);
-        firstPage.drawText(`${foundCertificate.dOLC}`, {
-            font: courierBoldFont,
-            size: 12,
-        });
-
-        firstPage.moveTo(350, 310);
-        firstPage.drawText(`${foundCertificate.yearOfExam}`, {
-            font: courierBoldFont,
-            size: 12,
-        });
-
-        firstPage.moveTo(350, 284);
-        firstPage.drawText(`${foundCertificate.resultDivision}`, {
-            font: courierBoldFont,
-            size: 12,
-        });
-
-        firstPage.moveTo(350, 258);
-        firstPage.drawText(`${foundCertificate.regNumber}`, {
-            font: courierBoldFont,
-            size: 12,
-        });
-
-        firstPage.moveTo(350, 231);
-        firstPage.drawText(`${foundCertificate.uniRollNumber}`, {
-            font: courierBoldFont,
-            size: 12,
-        });
-
-        firstPage.moveTo(350, 204);
-        firstPage.drawText(`${foundCertificate.remark}`, {
-            font: courierBoldFont,
-            size: 16,
-        });
-
-        firstPage.moveTo(85, 45);
-        firstPage.drawText(`: ${foundCertificate.dOLC}`, {
-            font: courierBoldFont,
-            size: 16,
-        });
+            let reversedDOAdm = reverseString(foundCertificate.dOAdm);
 
 
-        writeFileSync("RollNo_" + foundCertificate.uniRollNumber + "_CLC.pdf", await document.save());
-        let filename = "RollNo_" + foundCertificate.uniRollNumber + "_CLC.pdf"
-        
-        console.log('287');
-        
-        const filePath = path.join('D:/New BD College/BD College Site', filename);
-        
+            //Create pdf
+            const document = await PDFDocument.load(readFileSync("./CLC.pdf"));
+            const courierBoldFont = await document.embedFont(StandardFonts.Courier);
+            const firstPage = document.getPage(0);
 
-        if (existsSync(filePath)) {
-            // Send the PDF file as a download
-            res.download(filePath, `RollNo_${foundCertificate.uniRollNumber}_CLC.pdf`,  (err) => {
-                if (err) {
-                    console.error(err);
-                    res.status(500).send('An error occurred');
-                } else {
-                    // Delete the file after 10 minutes
-                    setTimeout(() => {
-                        unlink(filePath, (err) => {
-                            if (err) {
-                                console.error(`Error deleting file: ${err}`);
-                            } else {
-                                console.log(`File ${filePath} deleted`);
-                            }
-                        });
-                    }, 10 * 1000); // 10 minutes in milliseconds
-                }
+            firstPage.moveTo(105, 629);
+            firstPage.drawText(`${foundCertificate.serialNo}`, {
+                font: courierBoldFont,
+                size: 16,
             });
-        } else {
-            res.status(404).send('File not found');
-        }
+
+            firstPage.moveTo(431, 630);
+            firstPage.drawText(` ${foundCertificate.studentId}`, {
+                font: courierBoldFont,
+                size: 16,
+            });
+
+            firstPage.moveTo(350, 604);
+            firstPage.drawText(`${foundCertificate.fullName}`, {
+                font: courierBoldFont,
+                size: 12,
+            });
+
+            firstPage.moveTo(350, 576);
+            firstPage.drawText(`${foundCertificate.fatherName}`, {
+                font: courierBoldFont,
+                size: 12,
+            });
+
+            firstPage.moveTo(350, 550);
+            firstPage.drawText(`${foundCertificate.motherName}`, {
+                font: courierBoldFont,
+                size: 12,
+            });
+
+            firstPage.moveTo(350, 523);
+            firstPage.drawText(`${foundCertificate.aadharNumber}`, {
+                font: courierBoldFont,
+                size: 12,
+            });
+
+            firstPage.moveTo(350, 497);
+            firstPage.drawText(`${foundCertificate.parmanentAddress}`, {
+                font: courierBoldFont,
+                size: 12,
+            });
+
+            firstPage.moveTo(350, 471);
+            firstPage.drawText(`${reversedDOB}`, {
+                font: courierBoldFont,
+                size: 12,
+            });
+
+            firstPage.moveTo(350, 444);
+            firstPage.drawText(`${foundCertificate.course}`, {
+                font: courierBoldFont,
+                size: 12,
+            });
+
+            firstPage.moveTo(350, 417);
+            firstPage.drawText(`${foundCertificate.session}`, {
+                font: courierBoldFont,
+                size: 12,
+            });
+
+            firstPage.moveTo(350, 390);
+            firstPage.drawText(`${reversedDOAdm}`, {
+                font: courierBoldFont,
+                size: 12,
+            });
+
+            firstPage.moveTo(350, 363);
+            firstPage.drawText(`${foundCertificate.classRollNumber}`, {
+                font: courierBoldFont,
+                size: 12,
+            });
+
+            firstPage.moveTo(350, 336);
+            firstPage.drawText(`${foundCertificate.dOLC}`, {
+                font: courierBoldFont,
+                size: 12,
+            });
+
+            firstPage.moveTo(350, 310);
+            firstPage.drawText(`${foundCertificate.yearOfExam}`, {
+                font: courierBoldFont,
+                size: 12,
+            });
+
+            firstPage.moveTo(350, 284);
+            firstPage.drawText(`${foundCertificate.resultDivision}`, {
+                font: courierBoldFont,
+                size: 12,
+            });
+
+            firstPage.moveTo(350, 258);
+            firstPage.drawText(`${foundCertificate.regNumber}`, {
+                font: courierBoldFont,
+                size: 12,
+            });
+
+            firstPage.moveTo(350, 231);
+            firstPage.drawText(`${foundCertificate.uniRollNumber}`, {
+                font: courierBoldFont,
+                size: 12,
+            });
+
+            firstPage.moveTo(350, 204);
+            firstPage.drawText(`${foundCertificate.remark}`, {
+                font: courierBoldFont,
+                size: 16,
+            });
+
+            firstPage.moveTo(85, 45);
+            firstPage.drawText(`: ${foundCertificate.dOLC}`, {
+                font: courierBoldFont,
+                size: 16,
+            });
+
+
+            writeFileSync("RollNo_" + foundCertificate.uniRollNumber + "_CLC.pdf", await document.save());
+            let filename = "RollNo_" + foundCertificate.uniRollNumber + "_CLC.pdf"
+
+            // const filePath = path.join('D:/New BD College/BD College Site', filename);
+            const filePath = path.join(filename);
+
+
+            if (existsSync(filePath)) {
+                // Send the PDF file as a download
+                res.download(filePath, `RollNo_${foundCertificate.uniRollNumber}_CLC.pdf`, (err) => {
+                    if (err) {
+                        console.error(err);
+                        res.status(500).send('An error occurred');
+                    } else {
+                        // Delete the file after 10 minutes
+                        setTimeout(() => {
+                            unlink(filePath, (err) => {
+                                if (err) {
+                                    console.error(`Error deleting file: ${err}`);
+                                } else {
+                                    console.log(`File ${filePath} deleted`);
+                                }
+                            });
+                        }, 10 * 1000); // 10 minutes in milliseconds
+                    }
+                });
+            } else {
+                res.status(404).send('File not found');
+            }
         }
 
     } catch (error) {
