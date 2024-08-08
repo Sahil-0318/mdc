a24f4aa37b4a3e90e320a5a01965ffc85cc8c56c
Sahil Kumar <sahilkc3440@gmail.com>
2024-08-08 23:53:58 +0530

    tc

--- routes/certificatesRoutes/certificateAdminRouter.js
+++ routes/certificatesRoutes/certificateAdminRouter.js
@@ -4,11 +4,24 @@ import { adminAuth } from '../../middlewares/adminMiddleware.js'
 
 import {
     // Character Certificate
-    cclist, downloadcc
+    cclist, downloadcc,
+    // TC
+    tclist, downloadtc,
+    // Bonafied
+    bonafiedlist, downloadbonafied
 } from "../../controllers/certificateController/certificateAdminController.js"
 
+// Character certificate
 certificateAdminRouter.get("/cclist", adminAuth, cclist )
 certificateAdminRouter.get("/cc/:id", adminAuth, downloadcc )
+
+// TC
+certificateAdminRouter.get("/tclist", adminAuth, tclist )
+certificateAdminRouter.get("/tc/:id", adminAuth, downloadtc )
+
+// Bonafied
+certificateAdminRouter.get("/bonafiedlist", adminAuth, bonafiedlist )
+certificateAdminRouter.get("/bonafied/:id", adminAuth, downloadbonafied )
 
 
 export default certificateAdminRouter
