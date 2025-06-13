e664b95d39b6de653757f0afffff5440340494d1
Sahil Kumar <sahilkc3440@gmail.com>
2025-06-13 12:53:07 +0530

    edit complete

--- controllers/Ug_Reg_Sem_1_25_29_Controller/admin_Controller.js
+++ controllers/Ug_Reg_Sem_1_25_29_Controller/admin_Controller.js
@@ -60,12 +60,101 @@ export const ugRegSem1_25_29StudentDetails = async (req, res) => {
 
     const foundAdmForm = await Ug_reg_sem_1_25_29_adm_form.findOne({ _id: stuId })
     const foundStudent = await Ug_Reg_Sem_1_25_29_User.findOne({ _id: foundAdmForm.appliedBy })
-    console.log(foundAdmForm)
-    console.log(foundStudent)
 
     res.render('Ug_Reg_Sem_1_25_29/admStudentDetails', { foundAdmForm, foundStudent, user })
 
   } catch (error) {
     console.log("Error in ugRegSem1_25_29StudentDetails", error)
+  }
+}
+
+
+export const ugRegSem1_25_29EditStudentDetails = async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.id })
+    const { stuId } = req.params
+
+    const foundAdmForm = await Ug_reg_sem_1_25_29_adm_form.findOne({ _id: stuId })
+    const foundStudent = await Ug_Reg_Sem_1_25_29_User.findOne({ _id: foundAdmForm.appliedBy })
+
+    res.render('Ug_Reg_Sem_1_25_29/editStudentDetails', { foundAdmForm, foundStudent, user })
+
+  } catch (error) {
+    console.log("Error in ugRegSem1_25_29EditStudentDetails", error)
+  }
+}
+
+
+export const ugRegSem1_25_29EditStudentDetailsPost = async (req, res) => {
+  try {
+    let { studentName, fatherName, motherName, guardianName, referenceNumber, ppuConfidentialNumber, applicantId, course, email, paper1, paper2, paper3, paper4, paper5, paper6, dOB, gender, category, religion, familyAnnualIncome, bloodGroup, physicallyChallenged, maritalStatus, aadharNumber, mobileNumber, whatsAppNumber, address, district, policeStation, state, pinCode, examName, examBoard, examYear, examResult, obtMarks, fullMarks, obtPercent, session, admissionFee, paymentId, receiptNo } = req.body
+    const { stuId } = req.params
+
+    if (gender === "MALE") {
+      if (course === "Bachelor of Science" || paper1 === "Psychology") {
+        if (category === "GENERAL" || category === "BC-2") {
+          admissionFee = 2880
+        } else if (category === "BC-1") {
+          admissionFee = 2280
+        } else {
+          admissionFee = 1200
+        }
+      } else {
+        if (category === "GENERAL" || category === "BC-2") {
+          admissionFee = 2280
+        } else if (category === "BC-1") {
+          admissionFee = 1680
+        } else {
+          admissionFee = 600
+        }
+      }
+
+    } else {
+      if (course === "Bachelor of Science" || paper1 === "Psychology") {
+        admissionFee = 1200
+      } else {
+        admissionFee = 600
+      }
+    }
+
+    await Ug_reg_sem_1_25_29_adm_form.findOneAndUpdate({ _id: stuId }, { $set: { studentName, fatherName, motherName, guardianName, referenceNumber, ppuConfidentialNumber, applicantId, email, paper1, paper2, paper3, paper4, paper5, paper6, dOB, gender, category, religion, familyAnnualIncome, bloodGroup, physicallyChallenged, maritalStatus, aadharNumber, mobileNumber, whatsAppNumber, address, district, policeStation, state, pinCode, examName, examBoard, examYear, examResult, obtMarks, fullMarks, obtPercent, session, admissionFee, paymentId, receiptNo } })
+
+    const foundUserLogin = await Ug_reg_sem_1_25_29_adm_form.findOne({ _id: stuId })
+    await Ug_Reg_Sem_1_25_29_User.findOneAndUpdate({ _id: foundUserLogin.appliedBy }, { $set: { course, referenceNumber, mobileNumber } })
+
+    res.redirect('/ug-reg-sem-1-25-29-list')
+
+  } catch (error) {
+    console.log("Error in ugRegSem1_25_29EditStudentDetailsPost", error)
+  }
+}
+
+
+export const ugRegSem1_25_29AllExcelsheet = async (req, res) => {
+  try {
+    
+
+  } catch (error) {
+    console.log("Error in ugRegSem1_25_29AllExcelsheet", error)
+  }
+}
+
+
+export const ugRegSem1_25_29BAExcelsheet = async (req, res) => {
+  try {
+    
+
+  } catch (error) {
+    console.log("Error in ugRegSem1_25_29BAExcelsheet", error)
+  }
+}
+
+
+export const ugRegSem1_25_29BScExcelsheet = async (req, res) => {
+  try {
+    
+
+  } catch (error) {
+    console.log("Error in ugRegSem1_25_29BScExcelsheet", error)
   }
 }
--- routes/Ug_Reg_Sem_1_25_29_Route/admin_Route.js
+++ routes/Ug_Reg_Sem_1_25_29_Route/admin_Route.js
@@ -4,12 +4,22 @@ import jwt from 'jsonwebtoken'
 
 import { adminAuth } from '../../middlewares/adminMiddleware.js'
 
-import { ugRegSem1_25_29Password, ugRegSem1_25_29List, ugRegSem1_25_29StudentDetails } from '../../controllers/Ug_Reg_Sem_1_25_29_Controller/admin_Controller.js'
+import { ugRegSem1_25_29Password, ugRegSem1_25_29List, ugRegSem1_25_29StudentDetails, ugRegSem1_25_29EditStudentDetails, ugRegSem1_25_29EditStudentDetailsPost, ugRegSem1_25_29AllExcelsheet, ugRegSem1_25_29BAExcelsheet, ugRegSem1_25_29BScExcelsheet } from '../../controllers/Ug_Reg_Sem_1_25_29_Controller/admin_Controller.js'
 
 Ug_Reg_Sem_1_25_29_Admin_Router.get('/ug-reg-sem-1-25-29-password', adminAuth, ugRegSem1_25_29Password)
 
 Ug_Reg_Sem_1_25_29_Admin_Router.get('/ug-reg-sem-1-25-29-list', adminAuth, ugRegSem1_25_29List)
 
 Ug_Reg_Sem_1_25_29_Admin_Router.get('/ug-reg-sem-1-25-29-studentDetails/:stuId', adminAuth, ugRegSem1_25_29StudentDetails)
+
+Ug_Reg_Sem_1_25_29_Admin_Router.get('/ug-reg-sem-1-25-29-editStudentDetails/:stuId', adminAuth, ugRegSem1_25_29EditStudentDetails)
+
+Ug_Reg_Sem_1_25_29_Admin_Router.post('/ug-reg-sem-1-25-29-editStudentDetails/:stuId', adminAuth, ugRegSem1_25_29EditStudentDetailsPost)
+
+Ug_Reg_Sem_1_25_29_Admin_Router.get('/ug-reg-sem-1-25-29-all-excelsheet', adminAuth, ugRegSem1_25_29AllExcelsheet)
+
+Ug_Reg_Sem_1_25_29_Admin_Router.get('/ug-reg-sem-1-25-29-BA-excelsheet', adminAuth, ugRegSem1_25_29BAExcelsheet)
+
+Ug_Reg_Sem_1_25_29_Admin_Router.get('/ug-reg-sem-1-25-29-BSc-excelsheet', adminAuth, ugRegSem1_25_29BScExcelsheet)
 
 export default Ug_Reg_Sem_1_25_29_Admin_Router
